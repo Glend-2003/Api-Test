370003d0dde2a13330ea670acfef04ae74ef06e4
deyCedeno <162640178+deyCedeno@users.noreply.github.com>
2025-05-06 15:42:21 -0600

    Update Program.cs

--- ApiTest/Program.cs
+++ ApiTest/Program.cs
@@ -1,8 +1,15 @@
+using ApiTest.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//Build DatabaseContext
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
