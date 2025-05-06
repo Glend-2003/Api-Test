cafd6cfadaac33f3994934cc623777be884df189
deyCedeno <162640178+deyCedeno@users.noreply.github.com>
2025-05-06 15:34:35 -0600

    DbContexted created

---
+++ ApiTest/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTest.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options) { }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
